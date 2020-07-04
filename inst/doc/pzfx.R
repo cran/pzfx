0bffcd4146ad8d291e7cce7d7912f133ea1cb490
Yue Jiang <rivehill@gmail.com>
2020-07-04 03:20:03 +0000

    version 0.3.0

--- inst/doc/pzfx.R
+++ inst/doc/pzfx.R
@@ -1,22 +1,22 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(pzfx)
 pzfx_tables(system.file("extdata/exponential_decay.pzfx", package="pzfx"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df <- read_pzfx(system.file("extdata/exponential_decay.pzfx", package="pzfx"), table="Exponential decay")
 head(df)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df <- read_pzfx(system.file("extdata/exponential_decay.pzfx", package="pzfx"), table=1)
 head(df)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 tmp <- tempfile(fileext=".pzfx")
 write_pzfx(df, tmp, row_names=FALSE, x_col="Minutes")
 out_df <- read_pzfx(tmp, table=1)
